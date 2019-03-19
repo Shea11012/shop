20af1ba500439fa8a9cb4ace4033b24a1281d27c
shea <1872314654@qq.com>
2019-03-20 01:12:11 +0800

    update

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -2,13 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\OrderReviewed;
 use App\Exceptions\InvalidRequestException;
 use App\Http\Requests\OrderRequest;
-use App\Jobs\CloseOrder;
+use App\Http\Requests\SendReviewRequest;
 use App\Models\Order;
-use App\Models\ProductSku;
 use App\Models\UserAddress;
-use App\Services\CartService;
 use App\Services\OrderService;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
@@ -18,25 +17,79 @@ class OrdersController extends Controller
     public function index(Request $request)
     {
         $orders = Order::query()
-            ->with(['items.product','items.productSku'])
-            ->where('user_id',$request->user()->id)
-            ->orderBy('created_at','desc')
+            ->with(['items.product', 'items.productSku'])
+            ->where('user_id', $request->user()->id)
+            ->orderBy('created_at', 'desc')
             ->paginate();
 
-        return view('orders.index',compact('orders'));
+        return view('orders.index', compact('orders'));
     }
-    public function store(OrderRequest $request,OrderService $orderService)
+
+    public function store(OrderRequest $request, OrderService $orderService)
     {
         $user = $request->user();
         $address = UserAddress::find($request->input('address_id'));
 
-        return $orderService->store($user,$address,$request->input('remark'),$request->input('items'));
+        return $orderService->store($user, $address, $request->input('remark'), $request->input('items'));
     }
 
-    public function show(Order $order,Request $request)
+    public function show(Order $order)
     {
-        $this->authorize('own',$order);
-        $order = $order->load(['items.productSku','items.product']);
-        return view('orders.show',compact('order'));
+        $this->authorize('own', $order);
+        $order = $order->load(['items.productSku', 'items.product']);
+        return view('orders.show', compact('order'));
+    }
+
+    public function received(Order $order)
+    {
+        $this->authorize('own', $order);
+
+        if ($order->ship_status !== Order::SHIP_STATUS_DELIVERED) {
+            throw new InvalidRequestException('发货状态不正确');
+        }
+
+        $order->update(['ship_status' => Order::SHIP_STATUS_RECEIVED]);
+
+        return $order;
+    }
+
+    public function review(Order $order)
+    {
+        $this->authorize('own', $order);
+
+        if (!$order->paid_at) {
+            throw new InvalidRequestException('该订单未支付，不可评价');
+        }
+
+        return view('orders.review', ['order' => $order->load(['items.productSku', 'items.product'])]);
+    }
+
+    public function sendReview(Order $order, SendReviewRequest $request)
+    {
+        $this->authorize('own', $order);
+
+        if (!$order->paid_at) {
+            throw new InvalidRequestException('该订单未支付，不可评价');
+        }
+        if ($order->reviewed) {
+            throw new InvalidRequestException('该订单已评价，不可重复提交');
+        }
+        $reviews = $request->input('reviews');
+
+        \DB::transaction(function () use ($reviews, $order) {
+            foreach ($reviews as $review) {
+                $orderItem = $order->items()->find($review['id']);
+                $orderItem->update([
+                    'rating'      => $review['rating'],
+                    'review'      => $review['review'],
+                    'reviewed_at' => Carbon::now(),
+                ]);
+            }
+            $order->update(['reviewed' => true]);
+
+            event(new OrderReviewed($order));
+        });
+
+        return redirect()->back();
     }
 }
